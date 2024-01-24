4792cc3038422018cfabdb600d124f50348ba2ed
benkim077 <benkim0077@gmail.com>
2024-01-24 23:15:38 +0900

    Not Found 에러 처리 컴포넌트

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold py-4">컨텐츠가 존재하지 않습니다...</h1>
+      <Link href="/0">메인페이지로 이동</Link>
+    </>
+  );
+}
