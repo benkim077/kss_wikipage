9dd6dee08c7e4e1ce9235f7f66691552ab0e6e55
benkim077 <benkim0077@gmail.com>
2024-01-28 16:03:14 +0900

    메인 페이지로 이동 주소 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <>
       <h1 className="text-3xl font-bold py-4">컨텐츠가 존재하지 않습니다...</h1>
-      <Link href="/0">메인페이지로 이동</Link>
+      <Link href="/">메인페이지로 이동</Link>
     </>
   );
 }
